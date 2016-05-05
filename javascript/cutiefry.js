920cfc780ac5fc61f2df838b780d10431c47c91a
ennu0 <rooney.sarah5@yahoo.com>
2016-05-05 13:11:01 -0500

    top part of code is what i've edited in the bar so far, bottom part is me trying to edit the navagation links

---
+++ javascript/cutiefry.js
@@ -0,0 +1,13 @@
+var blueColor = document.querySelector("._2t-a._26aw._50ti._2s1y");
+	blueColor.style.backgroundColor = "#A8AFF0";
+	blueColor.style.backgroundImage = "none";
+	blueColor.style.borderBottom = "2px dotted #DADBED";
+	blueColor.style.boxShadow = "0 3px 3px 3px rgba(168,175,240, .5)";
+	blueColor.style.fontFamily = "monospace";
+	blueColor.style.color = "#696FBF";
+
+
+
+
+var navLinks = document.querySelector("");
+	navLinks.style.textShadow = "0 3px 3px 3px rgba(240,187,232 .5)";
